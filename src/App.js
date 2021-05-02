4a4da0ee553293192d72b1765f732d1ec6f705ed
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 21:38:13 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="app">
       <h1>Hello world! 🚀</h1>
+
+    {/*    Header   */}
+    {/*    Sidebar  */}
+    {/*    Recommended video    */}
     </div>
   );
 }
