f089edef40d678fbe5af5261d0c501fc1ef510da
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 21:51:21 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from "react";
 import "./styles/Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
 
 const Header = () => {
     return (
