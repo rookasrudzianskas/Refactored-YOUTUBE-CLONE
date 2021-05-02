22b15cce657c313eb4e4b4e15d107e3a9154c7f2
Rokas <rookas.rudzenskas@gmail.com>
2021-05-02 17:37:13 +0300

    Rokas

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAme8v38uPkjdxtx7TnYs94Joi30iiapPA",
+    authDomain: "clone-33b47.firebaseapp.com",
+    projectId: "clone-33b47",
+    storageBucket: "clone-33b47.appspot.com",
+    messagingSenderId: "752504209957",
+    appId: "1:752504209957:web:fe2460d2fea86b64fab5cc",
+    measurementId: "G-Z4NSLY9PJN"
+};
