663d422bd47b6a43b755da7014148572c8abdb60
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 07:13:34 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import './App.css';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import RecommendedVideos from "./components/RecommendedVideos";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+        <Router>
 
-    {/*    Header   */}
-        <Header />
-    {/*    Sidebar  */}
-        <div className="app__page">
-            <Sidebar />
 
-            <RecommendedVideos />
-            {/*    Recommended video    */}
-        </div>
+            {/*    Header   */}
+            <Header />
+
+            <Switch>
+                <Route path="/search">
+
+                </Route>
+                <Route path="/">
+
+                    {/*    Sidebar  */}
+                        <div className="app__page">
+                            <Sidebar />
+
+                            <RecommendedVideos />
+                            {/*    Recommended video    */}
+                        </div>
+
+                </Route>
+            </Switch>
+
+
+
+        </Router>
+
 
     </div>
   );
