1ba689041583649db688dbbea0e78ebf1a91b81d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 06:25:19 +0300

    Rokas

--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./styles/SidebarRow.css";
 import HomeIcon from "@material-ui/icons/Home";
 
-const SidebarRow = ({ Icon, title }) => {
+const SidebarRow = ({ selected, Icon, title }) => {
     return (
-        <div className="sidebarRow">
+        // if the sidebarRow has the class selected, so apply it to the first one
+        <div className={`sidebarRow ${selected && "selected"}`}>
             <Icon className="sidebarRow__icon" />
             <h2 className="sidebarRow__title">{title }</h2>
         </div>
